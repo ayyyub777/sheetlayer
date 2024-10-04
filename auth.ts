4273305f68aa69242606f356ff63bbdb4ca8ab8f
Ayoub Bigharassine <ayoub.bigharassine@gmail.com>
2024-10-04 18:32:13 +0100

    setup

---
+++ auth.ts
@@ -0,0 +1,42 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "@/prisma";
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+          scope:
+            "openid email profile https://www.googleapis.com/auth/spreadsheets",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      // @ts-ignore
+      session.accessToken = token.accessToken as string;
+      session.user.id = user.id;
+      return session;
+    },
+  },
+});
